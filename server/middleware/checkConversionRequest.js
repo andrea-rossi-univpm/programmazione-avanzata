a658749a297c75d5b37f52a7debc2eae2045eb6b
Andrea Rossi <s1104126@studenti.univpm.it>
2022-06-01 13:42:31 +0200

    using _PerformCall method of redis module: to check if user-mail can make a request (with its credit) and decrement actual credits

--- server/middleware/checkConversionRequest.js
+++ server/middleware/checkConversionRequest.js
@@ -3,9 +3,11 @@
 const CErrorFactory = require("../modules/error-factory");
 const errorFactory = new CErrorFactory();
 const enumHTTPStatusCodes = require("../models/httpsStatusCode");
+const redisHandler = require("../modules/redisHandler");
 
 
 const checkConversionRequest = function(req, res, next){
+
   const params =  req.body;
   if(!params || Object.keys(params).length === 0) {
     //why next does not work?
@@ -15,6 +17,31 @@ const checkConversionRequest = function(req, res, next){
     err.StatusCode = enumHTTPStatusCodes.BadRequest;
     require("./errorHandler")(err, req, res, null);
   } else {
+
+    //////////////////////////////////////////////////////////////////////
+    //before checking request source/dest, I would check If user can 
+    // perform the request: even if is a bad request, I'll decrement credit
+    // this to prevent Dos attacks consuming power on fake validation
+  
+    try {
+      const result = redisHandler._PerformCall(req.body.Email);
+      if(result) {
+        logger.LOG_INFO(
+          `User ${Email} spent 1 credit${CreditToAdd > 1 ? 's' : ''} to ${Email}`
+        );
+      }
+    } catch(ex) {
+      let err = new Error(
+        errorFactory.getError(enumHTTPStatusCodes.Unauthorized).getMsg() + `: ${ex}`
+      );
+      err.StatusCode = enumHTTPStatusCodes.Unauthorized;
+      require("./middleware/errorHandler")(err, req, res, null);
+      return;
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+
     if(!params.Source) {
       let err = new Error(
         errorFactory.getError(enumHTTPStatusCodes.BadRequest).getMsg() + ": Undefined system Source"
